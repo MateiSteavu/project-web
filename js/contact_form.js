cbf0fb3923958a19afec76a8151745d6ac8b1921
MateiSteavu <mateisteavu@yahoo.com>
2026-02-25 14:58:02 +0200

    feat: adaugă validare reală la formularul de contact

--- js/contact_form.js
+++ js/contact_form.js
@@ -13,13 +13,31 @@ else
     }
 
 
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Opreste reload-ul paginii
+ // ... codul vostru de validare ...
 
-function submitForm() {
+    const my_text = document.querySelector('footer p');
     const nume = document.getElementById("name").value; 
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
+    {
+        let ok = 1;
+        if(nume.length<2 || !email.includes('@') || message.length<10)
+            ok = 0;
+        if(ok){
+            my_text.textContent = 'A mers';
+            my_text.style.color = 'green';
+        }
+        else{
+            my_text.textContent = 'Nu a mers';
+            my_text.style.color = 'red';
+        }   
+    }
+    
 
     console.log(nume + " " + email + " " +message);
 
     console.warn("Goodbye World!");
-} 
+});
