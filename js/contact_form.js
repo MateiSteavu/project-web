65d955e9b351d12e71c3ca1c183c392cbe6863db
MateiSteavu <mateisteavu@yahoo.com>
2026-02-25 14:43:25 +0200

    feat: adaugă salut personalizat bazat pe ora zilei

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,3 +1,19 @@
+const Data = new Date();
+const my_text = document.querySelector('header p');
+
+if(Data.getHours()>=6 && Data.getHours()<12){
+    my_text.textContent = 'Bună dimineața! Bine ai venit pe pagina mea.';
+}
+else
+    if(Data.getHours()>=12 && Data.getHours()<18){
+        my_text.textContent = 'Bună ziua! Bine ai venit pe pagina mea.';
+    }
+    else{
+        my_text.textContent = 'Bună seara! Bine ai venit pe pagina mea.';
+    }
+
+
+
 function submitForm() {
     const nume = document.getElementById("name").value; 
     const email = document.getElementById("email").value;
