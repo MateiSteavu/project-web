a95e700a77488a503aade097725671d2f5ecf2d8
MateiSteavu <mateisteavu@yahoo.com>
2026-04-15 14:31:55 +0300

    Lab6 ex 2-3

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 function ProjectList() {
     const [projects, setProjects] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+    const [search] = useState('Pagina Personala');
     useEffect(function() {
         fetch('/data/projects.json')
             .then(function(response) {
@@ -12,15 +14,23 @@ function ProjectList() {
             .then(function(data) {
                 setProjects(data.projects);
                 setLoading(false);
+            }).catch(function(err) {
+            setError('Eroare la incarcarea datelor' + err);
+            setLoading(false);
             });
     }, []);
+    if(error);
     if (loading) {
+        
         return <p>Se incarca...</p>;
     }
     return (
         <div>
         <h3>Proiecte</h3>
-        {projects.map(function(item, index) {
+        {projects.filter(function(p) {
+            return p.title.toLowerCase().includes(search.toLowerCase());
+            })
+            .map(function(item, index) {
           return <Card key={index} title={item.title} description={item.tech} />;
         })}
         </div>
