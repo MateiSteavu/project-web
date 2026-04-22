184b457c1bd27957e7120db3bde94997af1294d7
MateiSteavu <mateisteavu@yahoo.com>
2026-04-22 14:09:36 +0300

    Lab7 ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,17 @@
-import Card from './Card';
-import QuickNote from './QuickNote';
-import { useState } from 'react';
-import TodoList from './ToDo';
-import ContactForm from './ContactForm';
-import Clock from './Clock';
-import ProjectList from './ProjectList';
-import WebAPI from './WebAPI';
-
-
-const projects = [
-  { title: "Proiect 1", description: "Pagina personala" },
-  { title: "Proiect 2", description: "Calculator buget" },
-  { title: "Proiect 3", description: "Dashboard React" },
-  { title: "Proiect 4", description: "Cea mai de buget chestie de pe aici, ca daca tot avem calculator buget..." },
-  { title: "Proiect 5", description: "Nu vreau sa scriu lorem, deci nu scriu lorem." },
-];
-// In JSX-ul returnat de App:
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
-  const [count, setCount] = useState(0);
-    return (
-        <div>
-        <h1>Dashboard</h1>
-        {/* <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" cartof="Alt test doar asa"/>
-        <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" cartof="Alt test doar asa"/>
-        <Card title="Proiect 3" description="Dashboard cu React" cartof="Alt test doar asa"/> */}
-
-        {projects.map(function(item, index) {
-          return <Card key={index} title={item.title} description={item.description} />;
-        })}
-        <p>Ai apasat de {count} ori</p>
-        <button onClick={() => setCount(count + 1)}>Click</button>
-        <br></br>
-        <button onClick={() => setCount(count - 1)}>-1</button>
-        <br></br>
-        <button onClick={() => setCount(0)}>Reset</button>
-        <QuickNote />
-        <TodoList />
-        <ContactForm />
-        <Clock />
-        <ProjectList />
-        <WebAPI />
-        </div>
-    );
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/projects" element={<Projects />} />
+    <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
